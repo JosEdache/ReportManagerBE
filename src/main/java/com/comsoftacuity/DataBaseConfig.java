7abc6506e29884d2a8efee5db2a5ab4c8a9b76aa
Joseph Edache <garapesz@gmail.com>
2018-10-04 21:11:05 +0100

    app.properties

--- src/main/java/com/comsoftacuity/DataBaseConfig.java
+++ src/main/java/com/comsoftacuity/DataBaseConfig.java
@@ -7,12 +7,14 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class DataBaseConfig {
 
 	@Bean
     @Primary
+    @Profile("production")
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
